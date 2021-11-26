7c828ac3810f70c74dfce8cdec33ea94fea05dd1
luis <luisalejandro.p.r@outlook.com>
2021-11-26 10:20:52 -0400

    Frist Commit

---
+++ src/ex_pf_1_tetris_draw/Ex_pf_1_tetris_draw.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex_pf_1_tetris_draw;
+
+/**
+ *
+ * @author luisa
+ */
+public class Ex_pf_1_tetris_draw {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
